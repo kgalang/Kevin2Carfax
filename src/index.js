0818167de11ce9574d77bbec56d908c6cf471a6d
kgalang <kgalang5@gmail.com>
2018-06-28 13:43:56 -0400

    Set up redux structure

--- src/index.js
+++ src/index.js
@@ -4,5 +4,35 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import thunk from 'redux-thunk';
+import {
+    applyMiddleware,
+    compose,
+    combineReducers,
+    createStore
+} from 'redux';
+import { Provider } from 'react-redux';
+
+//const allReducers = combineReducers({ });
+
+const allStoreEnhancers = compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension && window.devToolsExtension()
+)
+
+/*
+const store = createStore(
+    allReducers,
+    beginning state,
+    allStoreEnhancers
+);
+*/
+
+
+ReactDOM.render(
+    //<Provider store={store}>
+        <App />
+    //</Provider>
+    ,
+    document.getElementById('root'));
 registerServiceWorker();
